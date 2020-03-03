c8c14886ddb67387e5ea035bee287fd135c2117c
Nicole <barrosnicole094@gmail.com>
2020-03-03 20:11:14 -0300

    operadores aritméticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,24 @@
+#Soma
+print(2 + 3)
+
+#Subtração
+print(4 - 7)
+
+#Multiplicação
+print(2 * 5.3)
+
+#Divisão
+print( 9.4 / 3)
+ #Divisão obrigando a saída dde um número inteiro
+print(9.4 // 3)
+
+#Exponenciação: 2 elevado a oitava potência
+print(2 ** 8)
+
+#Módulo: Resto da divisão
+print(10 % 3)
+
+
+a = 12
+b = a
+print (a + b)
