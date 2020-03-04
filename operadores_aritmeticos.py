45aa181aeb712b35ce425b0372ee41a7cbb8091b
Nicole <barrosnicole094@gmail.com>
2020-03-04 09:46:22 -0300

    operadores relacionais

--- operadores_aritmeticos.py
+++ operadores_aritmeticos.py
@@ -22,3 +22,11 @@ print(10 % 3)
 a = 12
 b = a
 print (a + b)
+
+#Desafio: Calcule o percentual das despesas em cima do salário
+
+salario = 3450.45
+despesas = 2456.2
+
+percentual_comprometido = despesas / salario * 100
+print(percentual_comprometido)
---
+++ operadores_relacionais.py
@@ -0,0 +1,11 @@
+print(3 > 4)
+
+print(4 >= 3)
+
+print(3 <= 1)
+
+print(3 != 2)
+
+print(3 == 3)
+
+print(2 ==  '2')
