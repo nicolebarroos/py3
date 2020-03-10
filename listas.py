49ba2de11a7c423b0def01db2b2ed25a7928c759
Nicole <barrosnicole094@gmail.com>
2020-03-10 10:59:49 -0300

    listas

---
+++ listas.py
@@ -0,0 +1,41 @@
+lista[]
+type(lisa)
+dir (lista)
+
+len(lista)
+lista.append(1)
+lista.append(5)
+lista
+len(lista)
+
+#Listas podem ser modificadas
+nova_lista = [1, 5, 'Ana', 'Bia']
+nova_lista
+nova_lista.remove(5)
+nova_lista
+nova_lista.reverse()
+nova_lista
+
+#lista é indexada ou seja, começa do 0,1,2
+lista = [1, 5, 'Rebeca', 'Guilerme', 3.1415]
+lista.inde('Guilherme')
+lista[2]
+i in lista
+'Rebeca' in lista
+'Pedro' not in lista
+lista [0]
+lista[4]
+lista[-1]
+lista[-5]
+
+#pegando valores de uma lista
+lista = ['Ana', 'Lia', 'Rui', 'Paulo', 'Dani']
+lista[1:3]
+lista[1:-1]
+lista[1:]
+lista[:-1]
+lista[::2]
+lista[::-1]
+del lista[2]
+lista
+del lista[2:]
