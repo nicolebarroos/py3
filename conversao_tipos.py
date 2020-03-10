920b9cd4968f79c8b4edca0b90fc0a63cf357f42
Nicole <barrosnicole094@gmail.com>
2020-03-10 10:31:15 -0300

    mais fundamentos

---
+++ conversao_tipos.py
@@ -0,0 +1,13 @@
+a = 2
+b = '3'
+
+print(type(a))
+print(type(b))
+
+#converter tipo str em inteiro
+print (a + int(b))
+print(str(a) + b)
+
+type(str(a))
+
+print(2 + float('5.4'))
---
+++ operadores_logicos.py
@@ -0,0 +1,26 @@
+True or False
+
+#and
+7 != 3 and 2 > 3
+
+#Tabela Verdade do AND
+True and True
+True and False
+False and True
+False and False
+
+#Tabela verdade do OR
+True or True
+True or False
+False or True
+False or False
+
+#Tabela verdade do XOR
+True != True
+True != False
+False != True
+False != False
+
+#Operador de negação
+not True
+not False
