c4753c1ecf9f48c47ac7d6073f6bf752d3c0f515
Nicole <barrosnicole094@gmail.com>
2020-03-11 15:07:45 -0300

    projetos de fundamentos e controle

---
+++ fundamentos_projetos/area_circunferenciaV1.0.py
@@ -0,0 +1,5 @@
+pi = 3.14159
+area = float
+raio = float(input("Digite o raio: "))
+area = pi * (raio ** 2)
+print("Área da circunferência é: ", area)
---
+++ fundamentos_projetos/v4.py
@@ -0,0 +1,5 @@
+import math import pi
+
+raio = 15.3
+
+print('Área do círculo , ', math.pi * raio ** 2)
