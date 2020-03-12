66e179d069a3ab0a1985ccd067cdb86e0e73e634
Nicole <barrosnicole094@gmail.com>
2020-03-12 16:34:14 -0300

    Break, continue, switch, for com e sem else

---
+++ estruturas_controle/switch.py
@@ -0,0 +1,15 @@
+def get_dia_semana(dia):
+    dias = {
+        1: 'Domingo',
+        2: 'segunda',
+        3: 'Terça',
+        4: 'Quarta',   
+        5: 'Quinta',
+        6: 'Sexta',
+        7: 'Sábado',
+    }
+    return dias.get(dia, '** inválido ** ')
+
+if __name__ == '__main__':
+    for dia in range(0, 9):
+        print(f'{dia}: {get_dia_semana(dia)}')
